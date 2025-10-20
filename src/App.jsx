490666c990cff27ec525ec23fc0fcc8561f1f063
Pavithr Jain <pavithrjain19@gmail.com>
2025-10-20 15:39:45 +0530

    031

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import BackgroundAnimation from './components/BackgroundAnimation';
 import FireworksCanvas from './components/FireworksCanvas';
 
 export default function App() {
     return (
-        <FireworksCanvas />
+        <div className="relative w-full h-screen overflow-hidden">
+            <BackgroundAnimation />
+
+            <FireworksCanvas />
+        </div>
     )
 };
