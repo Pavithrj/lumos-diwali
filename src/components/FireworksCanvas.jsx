65b30d967b7a645adb32664aa950438e64001481
Pavithr Jain <pavithrjain19@gmail.com>
2025-10-20 13:45:07 +0530

    016

--- src/components/FireworksCanvas.jsx
+++ src/components/FireworksCanvas.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef } from 'react';
 
 export default function FireworksCanvas({ width, height }) {
     const canvasRef = useRef(null);
